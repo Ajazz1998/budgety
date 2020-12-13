9d8eae9649414af5da235b34e2adf223cc3e93e5
Ajay Bava <ajaybava1998@gmail.com>
2020-12-13 15:19:53 +0000

    commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 
 const budgetContoller = (function() {
-
+    var x = 2;
+    return x;
 })();
 
 const UIController = (function() {
-
+    var a = 2;
+    return a;
 })();
 
-const controller = (function() {
-
+const controller = (function(budgetCtrl, UICtrl) {
+    return {
+        pulicData: function(){
+            return budgetCtrl + UICtrl;
+        } 
+    } 
 })(budgetContoller, UIController);
